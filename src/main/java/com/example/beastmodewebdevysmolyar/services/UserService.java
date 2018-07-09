dd98cce98d4cad67b5247418c1644881c8f88841
ysmolyar <yonismolyar1@gmail.com>
2018-07-09 16:59:13 -0400

    added getmapping to service

--- src/main/java/com/example/beastmodewebdevysmolyar/services/UserService.java
+++ src/main/java/com/example/beastmodewebdevysmolyar/services/UserService.java
@@ -1,17 +1,32 @@
 package com.example.beastmodewebdevysmolyar.services;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 import com.example.beastmodewebdevysmolyar.models.User;
+import com.example.beastmodewebdevysmolyar.repositories.UserRepository;
 
 @RestController
 public class UserService {
 //	http://localhost:8080/register
 	
+	@Autowired
+	UserRepository userRepository;
+	
 	@PostMapping("/api/register")
 	public User register(@RequestBody User user) {
-		return user;
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return (List<User>) userRepository.findAll();
 	}
 }
