a8353abf79c7a29673b2a920294b794d9b5e967a
ysmolyar <yonismolyar1@gmail.com>
2018-07-10 16:42:27 -0400

    made user service client

---
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -0,0 +1,29 @@
+function UserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'http://localhost:8080/api/user';
+    var self = this;
+
+    function createUser(user, callback) {
+
+    }
+
+    function findAllUsers(callback) {
+
+    }
+
+    function findUserById(userId, callback) {
+        
+    }
+
+    function updateUser(userId, user, callback) {
+
+    }
+
+    function deleteUser(userId, callback) {
+
+    }
+}
