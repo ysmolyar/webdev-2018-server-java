9274dabc60bddf274ece5602cc1e0c22563b44f2
ysmolyar <yonismolyar1@gmail.com>
2018-07-11 16:23:20 -0400

    adjusted login directory

---
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -0,0 +1,33 @@
+(function () {
+    var $username,
+        $password,
+        $loginBtn;
+
+    function init() {
+        $username = $('#username');
+        $password = $('#password');
+        $loginBtn = $('#loginBtn');
+
+        $loginBtn.click(login);
+    }
+    init();
+
+    function login() {
+        var user = {
+            'username': $username.val(),
+            "password": $password.val()
+        };
+        fetch('/api/login', {
+            method: 'post',
+            body: JSON.stringify(user),
+            credentials: 'include',
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(navigateToProfile);
+    }
+
+    function navigateToProfile() {
+        window.location.href = '../profile/profile.template.client.html';
+    }
+})();
