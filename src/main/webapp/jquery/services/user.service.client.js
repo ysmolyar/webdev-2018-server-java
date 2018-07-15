e3a5570daa23ad78e1f4ff357e165f2e0c30593c
ysmolyar <yonismolyar1@gmail.com>
2018-07-14 21:09:15 -0400

    restored branch

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -7,23 +7,51 @@ function UserServiceClient() {
     this.url = 'http://localhost:8080/api/user';
     var self = this;
 
+
+    function updateUser() {
+        var user = {
+            username: $username.val(),
+            firstName: $firstName.val(),
+            lastName: $lastName.val()
+        };
+
+        fetch("/api/user/" + currentUser.id, {
+            method: 'put',
+            body: JSON.stringify(user),
+            'credentials': 'include',
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+
+    function deleteUser(id) {
+        var url = "/api/user/" + id;
+
+        return fetch(url, {
+            method: 'delete'
+        })
+    }
+
+    function findAllUsers() {
+        var url = "/api/user";
+        return fetch(url)
+            .then(function (response) {
+                return response.json();
+            });
+    }
+
+    function findUserById(userId) {
+        return fetch('/api/user/' + userId)
+            .then(function (response) {
+                return response.json();
+            });
+    }
+
+
     function createUser(user, callback) {
 
     }
 
-    function findAllUsers(callback) {
 
-    }
-
-    function findUserById(userId, callback) {
-        
-    }
-
-    function updateUser(userId, user, callback) {
-
-    }
-
-    function deleteUser(userId, callback) {
-
-    }
 }
