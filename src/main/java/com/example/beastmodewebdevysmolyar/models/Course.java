4ba8c26d3c7f11fc7f0976bcf6c077b7ed43a7e5
ysmolyar <yonismolyar1@gmail.com>
2018-07-18 01:28:29 -0400

    modified course

--- src/main/java/com/example/beastmodewebdevysmolyar/models/Course.java
+++ src/main/java/com/example/beastmodewebdevysmolyar/models/Course.java
@@ -1,11 +1,13 @@
 package com.example.beastmodewebdevysmolyar.models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -19,6 +21,8 @@ public class Course {
     private Date created;
     @Temporal(TemporalType.TIMESTAMP)
     private Date modified;
+    @OneToMany(mappedBy="course")
+    private List<Module> modules;
     
     
 	public int getId() {
