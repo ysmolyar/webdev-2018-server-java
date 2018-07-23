ae24c0ec7074e64a260d230b1bc73b90b2ec3a66
ysmolyar <yonismolyar1@gmail.com>
2018-07-23 03:49:29 -0400

    added entity tag to module to fix error

--- src/main/java/com/example/beastmodewebdevysmolyar/models/Module.java
+++ src/main/java/com/example/beastmodewebdevysmolyar/models/Module.java
@@ -2,6 +2,7 @@ package com.example.beastmodewebdevysmolyar.models;
 
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@Entity
 public class Module {
 
 	@Id
